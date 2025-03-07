de7930d4d5bc8ed1a5eb7855a5b8c279474a32e2
lenye uwaeme <lenyeuwaeme@gmail.com>
2025-03-07 16:28:42 -0500

    inital commit

---
+++ knownledge_base.py
@@ -0,0 +1,15 @@
+class KnownledgeBase:
+    def __init__(self,doc_path) -> None: #doc_path is the where all the source documents will be stored.
+        self.doc_path=doc_path
+
+    def load_docs():  #Loads document
+        pass
+
+    def split_doc():  #function to split docs
+        pass
+
+    def convert_to_embeddings(): #Create embeddings for vector database
+        pass
+
+    def retriver(): #Loads the retriver
+        pass
